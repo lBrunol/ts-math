6b82989a6ea6b1a0865ed09a4b1bbe5f47d08a1c
Bruno Araújo <bruno.ar.avl@gmail.com>
2022-03-23 12:31:00 -0300

    fix: validate number values in some function

--- src/math.ts
+++ src/math.ts
@@ -1,18 +1,48 @@
-const sum = (addendOne: number, addendTwo: number): number => addendOne + addendTwo;
+const sum = (addendOne: number, addendTwo: number): number => {
+  if(typeof addendOne === "number" && typeof addendTwo === "number"){
+    return addendOne + addendTwo;
+  }
+  return NaN;
+};
 
-const sumAny = (addendOne: any, addendTwo: any): number => parseFloat(addendOne) + parseFloat(addendTwo)
+const sumAny = (addendOne: any, addendTwo: any): number => parseFloat(addendOne) + parseFloat(addendTwo);
 
-const substract = (minuend: number, subtrahend: number): number => minuend - subtrahend;
+const substract = (minuend: number, subtrahend: number): number => {
+  if(typeof minuend === "number" && typeof subtrahend === "number"){
+    return minuend - subtrahend;
+  }
+  return NaN;
+};
 
-const multiply = (factorOne: number, factorTwo: number): number => factorOne * factorTwo;
+const multiply = (factorOne: number, factorTwo: number): number => { 
+  if(typeof factorOne === "number" && typeof factorTwo === "number"){  
+    return factorOne * factorTwo;
+  }
+  return NaN;
+};
 
 const divide = (dividend: number, divisor: number): number => dividend / divisor;
 
-const root = (x: number) => Math.sqrt(x);
+const root = (x: number) => {
+  if(typeof x === "number"){  
+    return Math.sqrt(x);
+  }
+  return NaN;
+}
 
-const powerOf = (base: number, expoent: number): number => Math.pow(base, expoent);
+const powerOf = (base: number, expoent: number): number => { 
+  if(typeof base === "number" && typeof expoent === "number"){  
+    return Math.pow(base, expoent);  
+  }
+  return NaN;
+};
 
-const cosine = (radian: number) => Math.cos(radian);
+const cosine = (radian: number) => {
+  if(typeof radian === "number"){  
+    return Math.cos(radian);
+  }
+  return NaN;
+};
 
 const hypotenuse = (a?: number, b?: number) => {
   if (typeof a === "number" && typeof b === "number") {
