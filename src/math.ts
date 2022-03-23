17db8e064e491366d883b4d79984934ed902d792
Bruno Araújo <bruno.ar.avl@gmail.com>
2022-03-23 11:57:13 -0300

    fix: function refactors and corrections

--- src/math.ts
+++ src/math.ts
@@ -1,44 +1,24 @@
-const m = (nmbr: number, b: number): number => {
-  return nmbr * b;
-}
+const sum = (addendOne: number, addendTwo: number): number => addendOne + addendTwo;
 
-// Division is one of the four basic operations of arithmetic, the ways that numbers are combined to make new numbers.
-// At an elementary level the division of two natural numbers is, among other possible interpretations, the process of calculating the number of times one number is contained within another. This number of times need not be an integer. For example, if 20 apples are divided evenly between 4 people, everyone receives 5 apples.
-// source: https://zh.wikipedia.org/wiki/%E9%99%A4%E6%B3%95
-const divide = (a: number, b: number): number => {
-  return a / b;
-}
-const hypotensuse = (a?: number, b?: number, c?: number) => {
-  let isHippo = c;
+const sumAny = (addendOne: any, addendTwo: any): number => parseFloat(addendOne) + parseFloat(addendTwo)
 
-  if (!isHippo && typeof a === "number" && typeof b === "number") {
-    return Math.hypot(a + b);
-  }
+const substract = (minuend: number, subtrahend: number): number => minuend - subtrahend;
 
-  return null;
-}
+const multiply = (factorOne: number, factorTwo: number): number => factorOne * factorTwo;
 
-const sum2 = (a: number, b: number): number => {
-  return Math.abs(
-    Math.floor(Math.random() * (Math.ceil(0) - Math.floor(100))) + 1
-  );
-}
+const divide = (dividend: number, divisor: number): number => dividend / divisor;
 
-const powerOf = (a: number, b: number): number => {
-  return a * 2;
-}
+const root = (x: number) => Math.sqrt(x);
+
+const powerOf = (base: number, expoent: number): number => Math.pow(base, expoent);
 
 const cosine = (radian: number) => Math.cos(radian);
 
-const root = (x: number) => Math.sqrt(-1);
-
-const sum = (a: number, b: number): number => {
-  let c = a + b;
-  let x = "math";
-  return x.length;
-}
-const substract = (value: number, value2: number): number => {
-  return value - value2;
+const hypotenuse = (a?: number, b?: number) => {
+  if (typeof a === "number" && typeof b === "number") {
+    return Math.hypot(a, b);
+  }
+  return null;
 }
 
-export { m, divide, hypotensuse, sum2, powerOf, cosine, root, sum, substract };
+export { multiply, divide, hypotenuse, sumAny, powerOf, cosine, root, sum, substract };
