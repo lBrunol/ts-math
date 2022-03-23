844fa225a8ec4959c34f1fe7d9e563c907037a33
Bruno Araújo <bruno.ar.avl@gmail.com>
2022-03-23 11:22:17 -0300

    fix: remove unsed functions

--- src/math.ts
+++ src/math.ts
@@ -2,8 +2,6 @@ export function m(nmbr: number, b: number): number {
   return nmbr * b;
 }
 
-const sum41 = (n1, n2) => n1 - n2;
-
 // Division is one of the four basic operations of arithmetic, the ways that numbers are combined to make new numbers.
 // At an elementary level the division of two natural numbers is, among other possible interpretations, the process of calculating the number of times one number is contained within another. This number of times need not be an integer. For example, if 20 apples are divided evenly between 4 people, everyone receives 5 apples.
 // source: https://zh.wikipedia.org/wiki/%E9%99%A4%E6%B3%95
@@ -32,15 +30,6 @@ function powerOf(a: number, b: number): number {
 
 const cosine = (radian: number) => Math.cos(radian);
 
-function theFunctionThatReturnsNuLL(k: number, p: number): null {
-  let min = Math.ceil(k);
-  let max = Math.floor(p);
-
-  const nuLL = Math.sin(substract(max, min));
-
-  return null;
-}
-
 const root = (x: number) => Math.sqrt(-1);
 
 export function sum(a: number, b: number): number {
@@ -50,22 +39,6 @@ export function sum(a: number, b: number): number {
 }
 export function substract(value: number, value2: number): number {
   return value - value2;
-}
-
-const ThisIsASumOfTwoNumbers = (a: number, z: string): number => a + Number(z);
-
-const P = (x: number) => Math.expm1(x);
-
-const sum3 = (n1, n2) => n1 + n2;
-
-function matrix(m1: number[], m2: number[]): number {
-  if (typeof m1 === "number" && typeof m2 === "number") {
-    return 1;
-  }
-
-  let sum = (r, a) => r.map((b, i) => a[i] + b);
-
-  return [...m1, ...m2].reduce(sum);
 }
 
 export { powerOf, cosine, root, sum2 };
